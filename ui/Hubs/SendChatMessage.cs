32913eb992312b881c4e0060dea719bc082b8f6b
Éric Talbot <eric.talbot@groupecanam.com>
2014-12-08 11:57:42 -0500

    initial commit

---
+++ ui/Hubs/SendChatMessage.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Microsoft.AspNet.SignalR;
+
+namespace ui.Hubs
+{
+    public class SendChatMessage : ISendChatMessage
+    {
+  
+        #region properties
+
+        private IHubContext _chatHubContext;
+ 
+        #endregion
+
+        #region ctor
+
+        public SendChatMessage()
+        {
+            _chatHubContext = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+        }
+
+        #endregion
+
+        #region public methods
+
+        public void SendMessageToUser(string windowsAccountName, string message)
+        {
+            User user = SignalRConnections.Instance.GetUser(windowsAccountName);
+            if (user != null)
+                user.ConnectionIds.ToList().ForEach(c => _chatHubContext.Clients.Client(c).receive(message));
+        }
+
+        public void SendToAll(string message)
+        {
+            SignalRConnections.Instance.Users.ToList().ForEach(u =>
+                u.Value.ConnectionIds.ToList().ForEach(c =>
+                    _chatHubContext.Clients.Client(c).receive(message)));
+        }
+
+        #endregion
+
+    }
+}
